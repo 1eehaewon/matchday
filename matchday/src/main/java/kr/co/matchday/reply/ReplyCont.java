a1ef42fd7e9774dec7702e8a56a742195c195845
itwill <itwill@DESKTOP-I2V69VB>
2024-07-12 17:09:24 +0900

    2024-07-12 강현모

--- matchday/src/main/java/kr/co/matchday/reply/ReplyCont.java
+++ matchday/src/main/java/kr/co/matchday/reply/ReplyCont.java
@@ -35,7 +35,7 @@ public class ReplyCont {
         ReplyDTO replyDto = new ReplyDTO();
         replyDto.setNoticeid(noticeid);
         replyDto.setContent(content);        
-        replyDto.setUserid("itwill");
+        replyDto.setUserid("webmaster");
         //->로그인 기능을 구현했거나 따로 댓글 작성자를 입력받는 폼이 있다면 입력 받아온 값으로 사용하면 된다.
         //->예) session.getAttribute("s_id") 활용
         //->여기서는 따로 폼을 구현하지 않았기 때문에 임시로 "test" 라 한다
